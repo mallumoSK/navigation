3f40b8586b788dc5b090d79f408411504c5d7ef0
mallumo <develop.mallumo@gmail.com>
2021-01-20 20:50:28 +0100

    v0.1.7 navigation: - compose nav preview with args by:
    val argsClass = remember { ComeArgsClass() }
    Providers(AmbientNavigation provides Navigation.preview(argsClass)) {
     // rest of preview code
    }

--- navigation/src/main/java/tk/mallumo/compose/navigation/Navigation.kt
+++ navigation/src/main/java/tk/mallumo/compose/navigation/Navigation.kt
@@ -2,9 +2,11 @@
 
 package tk.mallumo.compose.navigation
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.ambientOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.viewinterop.viewModel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
@@ -13,11 +15,11 @@ import kotlin.reflect.KClass
 val AmbientNavigation = ambientOf<Navigation> { error("Unexpected error") }
 
 @Deprecated(
-    message = "Old type of declaration",
-    replaceWith = ReplaceWith(
-        "AmbientNavigation",
-        "tk.mallumo.compose.navigation.AmbientNavigation"
-    )
+        message = "Old type of declaration",
+        replaceWith = ReplaceWith(
+                "AmbientNavigation",
+                "tk.mallumo.compose.navigation.AmbientNavigation"
+        )
 )
 val NavigationAmbient
     get() = AmbientNavigation
@@ -29,29 +31,29 @@ data class Node(val id: String) {
 
 @Composable
 fun <VM : ViewModel> navigationViewModel(
-    modelClass: KClass<VM>,
-    factory: ViewModelProvider.Factory? = null
+        modelClass: KClass<VM>,
+        factory: ViewModelProvider.Factory? = null
 ): VM {
 
     val nodeID = AmbientNavigation.current.nodeIdentifier
     val viewModelKey = "$nodeID${modelClass.qualifiedName}"
 
     viewModel(ImplNavigationViewModel::class.java)
-        .nodeViewModelRegister(viewModelKey)
+            .nodeViewModelRegister(viewModelKey)
 
     return viewModel(
-        modelClass = modelClass.java,
-        key = viewModelKey,
-        factory = factory
+            modelClass = modelClass.java,
+            key = viewModelKey,
+            factory = factory
     )
 }
 
 
 class Navigation constructor(
-    private val navigationViewModel: ImplNavigationViewModel,
-    val args: Bundle,
-    val nodeIdentifier: String,
-    private val bundledCallback: () -> Any?
+        private val navigationViewModel: ImplNavigationViewModel,
+        val args: Bundle,
+        val nodeIdentifier: String,
+        private val bundledCallback: () -> Any?
 ) {
 
     /**
@@ -65,6 +67,17 @@ class Navigation constructor(
             get() = Navigation(ImplNavigationViewModel(), Bundle(), "-") {
                 null
             }
+
+        @SuppressLint("ComposableNaming")
+        @Composable
+        fun preview(args: Any) {
+            remember(args) {
+                Navigation(ImplNavigationViewModel(), Bundle(), "-") {
+                    args
+                }
+            }
+
+        }
     }
 
     init {
