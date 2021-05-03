840aa07edd7d6c602e880776a32eba981d9a76f2
mallumo <develop.mallumo@gmail.com>
2021-05-03 21:48:05 +0200

    SwipeToDismissOverlay swipe navigation for old devices

---
+++ navigation/src/main/java/tk/mallumo/compose/navigation/SwipeToDismissOwerlay.kt
@@ -0,0 +1,123 @@
+package tk.mallumo.compose.navigation
+
+
+import android.os.Build
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.BlendMode
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.dp
+import kotlin.math.roundToInt
+
+
+private enum class SwipeState {
+    START, END
+}
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun SwipeToDismissOverlay(
+    modifier: Modifier = Modifier,
+    isEnabled: Boolean = Build.VERSION.SDK_INT < 29,
+    onDismiss: () -> Unit, body: @Composable BoxScope.() -> Unit
+) {
+
+    val screenWidth = LocalConfiguration.current.screenWidthDp.dp
+
+    val sweepableState = rememberSwipeableState(SwipeState.START,
+        confirmStateChange = { newState ->
+            onDismiss.takeIf { newState == SwipeState.END }?.invoke()
+            false
+        })
+
+
+    val dest = LocalDensity.current
+
+    val dimensions = remember {
+        val end = with(dest) { (screenWidth.toPx() / 8F) }
+        val start = with(dest) { -(end / 4F).dp.toPx() }
+        start to end
+
+    }
+
+    val anchors = remember {
+        mapOf(
+            dimensions.first to SwipeState.START,
+            dimensions.second to SwipeState.END
+        )
+    }
+
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .swipeable(
+                state = sweepableState,
+                anchors = anchors,
+                thresholds = { _, _ -> FixedThreshold(dimensions.second.dp) },
+                resistance = ResistanceConfig(dimensions.second, dimensions.second, dimensions.second),
+                orientation = Orientation.Horizontal
+            )
+    ) {
+        body()
+        if (isEnabled) {
+            DismissLine(
+                modifier = Modifier.align(Alignment.CenterStart),
+                offset = sweepableState.offset,
+                offsetMax = dimensions.second
+            )
+        }
+    }
+}
+
+@Composable
+private fun DismissLine(
+    modifier: Modifier,
+    offset: State<Float>,
+    offsetMax: Float
+) {
+    val dest = LocalDensity.current
+
+    val onePercent = remember {
+        offsetMax / 100F
+    }
+
+    val pathColor = contentColorFor(backgroundColor = LocalContentColor.current)
+
+    Canvas(modifier = modifier
+        .size(24.dp)
+        .offset { IntOffset(offset.value.roundToInt(), 0) }) {
+        val width = size.width
+        val height = size.height
+        val center = height / 2F
+
+        if (offset.value > 0F) {
+            val lineOffset = ((offset.value / dest.density) / onePercent) / 100F
+
+            val path = Path().apply {
+                moveTo(lineOffset * width, 0F)
+                lineTo(0F, center)
+                lineTo(lineOffset * width, height)
+            }
+            drawPath(
+                path = path,
+                alpha = 0.9F,
+                color = pathColor,
+                blendMode = BlendMode.SrcOver,
+                style = Stroke(
+                    width = 5F
+                )
+            )
+        }
+    }
+}
